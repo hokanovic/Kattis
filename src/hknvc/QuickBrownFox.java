f3e4cfabd3089306927c043be4069f8cab9b7758
Håkan Ahlström <hakan.ahlstrom@live.se>
2018-06-20 10:45:25 +0200

    Accepted

--- src/hknvc/QuickBrownFox.java
+++ src/hknvc/QuickBrownFox.java
@@ -5,24 +5,26 @@ import java.util.Scanner;
 public class QuickBrownFox {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String phrase = sc.nextLine().toLowerCase();
-        StringBuilder missingLetters = new StringBuilder();
-        //97-122
-        boolean isPangram = true;
-        for (int i = 97; i < 123; i++) {
-            String Chr = (char) i+"";
-            boolean contains = phrase.contains(Chr);
-            if (!contains) {
-                missingLetters.append(Chr);
-                isPangram = false;
+        int repeat = sc.nextInt();
+        sc.nextLine();
+        for (int x = 0; x < repeat; x++) {
+            String phrase = sc.nextLine().toLowerCase();
+            StringBuilder missingLetters = new StringBuilder();
+            boolean isPangram = true;
+            for (int i = 97; i < 123; i++) {
+                char chr = (char) i;
+                boolean contains = phrase.indexOf(chr) >= 0;
+                if (!contains) {
+                    missingLetters.append(chr);
+                    isPangram = false;
+                }
+            }
+            System.out.println();
+            if (isPangram) {
+                System.out.println("Pangram");
+            } else {
+                System.out.println("missing " + missingLetters.toString());
             }
         }
-        if (isPangram) {
-            System.out.println("Pangram");
-        }
-        else {
-            System.out.println("missing " + missingLetters.toString());
-        }
-
     }
 }
