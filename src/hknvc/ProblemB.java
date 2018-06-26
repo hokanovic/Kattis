01e4e31f9e7a3d3952335a9fc44cdba29bfbf080
Håkan Ahlström <hakan.ahlstrom@live.se>
2018-06-26 15:28:49 +0200

    Accepted

--- src/hknvc/ProblemB.java
+++ src/hknvc/ProblemB.java
@@ -1,59 +1,61 @@
 package hknvc;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Set;
 
 public class ProblemB {
     public static void main(String[] args) {
 
-        List<String> strList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-
             int repeat = Integer.parseInt(sc.nextLine().trim());
             if (repeat == 0) {
                 break;
             }
 
-            Set<String> strSet = new HashSet<>();
+            HashMap<String, Integer> map = new HashMap<>();
             for (int i = 0; i < repeat; i++) {
                 String next = sc.nextLine();
-                strList.add(next);
-                strSet.add(next);
+                mapAdd(next, map);
             }
-            int unique = strSet.size();
-            int collision = calcCollisions(strList);
 
+            int unique = map.size();
+            int collision = calcCollisions(map);
             System.out.println(unique + " " + collision);
         }
+
     }
 
-    private static int calcCollisions(List<String> strList) {
-        Set<String> set = new HashSet<>();
-        int counter = 0;
-        for (int i = 0; i < strList.size() - 1; i++) {
-            for (int j = i + 1; j < strList.size(); j++) {
-                String next = hash(strList.get(i), strList.get(j));
-                set.add(next);
-                counter++;
+    private static void mapAdd(String next, HashMap<String, Integer> map) {
+        if (map.containsKey(next)) {
+            map.put(next, map.get(next) + 1);
+        } else {
+            map.put(next, 1);
+        }
+    }
+
+    private static int calcCollisions(HashMap<String, Integer> strList) {
+        int collisions = 0;
+        for (String str1 : strList.keySet()) {
+            char hash1 = hash(str1);
+            for (String str2 : strList.keySet()) {
+                if (!str1.equals(str2)) {
+                    char hash2 = hash(str2);
+                    if (hash1 == hash2)
+                        collisions += strList.get(str1) * strList.get(str2);
+                }
             }
         }
-        return (counter - set.size());
+        return collisions / 2;
     }
 
-    private static String hash(String strFirst, String strSecond) {
-        StringBuilder sb = new StringBuilder();
-        int repeat = Math.min(strFirst.length(), strSecond.length());
-        for (int i = 0; i < repeat; i++) {
-            sb.append((char) (strFirst.charAt(i) ^ strSecond.charAt(i)));
-        }
-        return sb.toString();
+    private static char hash(String str) {
+        char xor = (char) 0;
+        for (int i = 0; i < str.length(); i++)
+            xor ^= str.charAt(i);
+        return xor;
     }
-
 }
 
 
